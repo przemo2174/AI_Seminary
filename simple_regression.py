baf5bebb5367b4210e9fd2443e3a77d14b79863c
Przemek <przemo217@hotmail.com>
2018-04-17 23:00:47 +0200

    changes

--- simple_regression.py
+++ simple_regression.py
@@ -6,12 +6,12 @@ import pandas as pd
 def predict_sales(radio, weight, bias):
     return weight * radio + bias
 
+
 def cost_function(radio, sales, weight, bias):    
     companies = len(radio)
-    total_error = 0.0
-    for i in range(companies):
-        total_error += (sales[i] - (weight*radio[i] + bias))**2
+    total_error = ((np.array(sales) - np.array(predict_sales(radio, weight, bias))) ** 2).sum()
     return total_error / companies
+
 
 def update_weights(radio, sales, weight, bias, learning_rate):
     weight_deriv = 0
@@ -45,11 +45,11 @@ def train(radio, sales, weight, bias, learning_rate, iters):
         # Log Progress
         if i % 10 == 0:
             print("iter: "+str(i) + " cost: "+str(cost))
+            plt.scatter(radio, sales)
+            plt.plot(radio, [y * weight + bias for y in radio])
+            plt.show()
 
-        # plt.scatter(radio, sales)
-        # plt.plot(radio, [y * weight + bias for y in radio])
-        # plt.show()
-
+        
     return weight, bias, cost_history
 
 df = pd.read_csv('Advertising.csv')
@@ -57,15 +57,26 @@ df = pd.read_csv('Advertising.csv')
 y = df['sales']
 x = df['radio']
 
+weight = 0.3
+bias = 0.014
+
+# for i in range(0, 30):
+#     print('Iter:', i, 'Weight:', weight, 'Bias:', bias)
+#     print('Cost:', cost_function(x, y, weight, bias))
+#     weight,bias = update_weights(x, y, weight, bias, 0.001)
+
 plt.scatter(x, y)
 plt.xlabel('Radio ($)')
 plt.ylabel('Sales (unit)')
 # plt.xlim(xmin=-5, xmax=60)
 # plt.ylim(ymin=-5, ymax=50)
-plt.plot(x, predict_sales(x, np.random.randn(), np.random.randn()))
+plt.plot(x, predict_sales(x, 0.45, 0.025))
 plt.show()
 
-# weight, bias, cost_history = train(x, y, np.random.randn(), np.random.randn(), 0.1, 500)
+print(cost_function(x, y, 0.45, 0.025))
+
+weight, bias, cost_history = train(x, y, weight, bias, 0.001, 30)
+print(weight, bias, cost_history)
 
 # plt.scatter(x, y)
 # plt.plot(x, x * weight + bias)
@@ -76,8 +87,9 @@ plt.show()
 # def error_function(wags, wags_fit):
 #     return ((np.array(wags) - np.array(wags_fit)) ** 2).sum() / len(wags)
 
-# pats = [1, 2, 4]
-# wags = [2, 4, 5]
+ #pats = [1, 2, 4]
+ #wags = [2, 4, 5]
+
 
 # w = np.random.randn()
 # b = np.random.randn()
